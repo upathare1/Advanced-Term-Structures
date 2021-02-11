5672813146fecca7c3e2959cbe8991a1d2478b5e
upathare <upapilot@gmail.com>
2021-02-10 22:07:03 -0800

    Refactor new MC code and construct yield curve

--- Code/evaluators/monte_carlo.py
+++ Code/evaluators/monte_carlo.py
@@ -5,80 +5,61 @@ import pandas as pd
 from IPython.display import display
 
 class MonteCarlo:
-    """Class for running MC experiments"""
-    def __init__(self, spot_rate_model, r, t, T, m, n):
-        self.spot_rate_model = spot_rate_model
-        self.t = t
-        self.T = T
-        self.m = m
-        self.n = n
-        self.r = r
-
-    def _is_integer(self, fl: float):
-        """Helper function to check if float is an integer"""
-        return fl.is_integer()
-
-    def _simulate_path(self):
-        """Simulate a single path"""
-        r = [self.r]
-        n = []
-        p = []
-        J = []
-        delta_t = (self.T-self.t)/self.m
-        for j in range(self.m):
-            rj1, nj, pj, Jj = self.spot_rate_model.increment(r[j], self.t, self.T, self.m)
-            r.append(rj1)
-            n.append(nj)
-            p.append(pj)
-            J.append(Jj)
-        prices = np.exp(-np.cumsum(np.multiply(r[1:], delta_t)))
-        prices = np.insert(prices, 0, np.exp(-self.t*self.r))
-        return prices, r, n, p, J
-    
-    def _simulate_antithetical_path(self, n, p, J):
-        """Simulate antithetical path"""
-        n_anti = np.negative(n)
-        J_anti = np.subtract(2*self.spot_rate_model.model_params["mu"], J)
-        p_anti = p
-        r = [self.r]
-        delta_t = (self.T-self.t)/self.m
-        for j in range(self.m):
-            rj1, _, __, ___ = self.spot_rate_model.increment(r[j], self.t, self.T, self.m, n_anti[j], p_anti[j], J_anti[j])
-            r.append(rj1)
-        prices = np.exp(-np.cumsum(np.multiply(r[1:], delta_t)))
-        prices = np.insert(prices, 0, np.exp(-self.t*self.r))
-        return prices, r
-    
-    def _simulate_price(self):
-        """Classical MC simulation"""
-        prices = np.empty((self.n, self.m+1), dtype=np.float)
-        rs = np.empty((self.n, self.m+1), dtype=np.float)
-        for i in range(self.n):
-            prices[i, :], rs[i, :], _, __, ___ = self._simulate_path()
-        return prices, rs
-    
-    def _simulate_price_antithetical(self):
-        """Antithetical Variates MC"""
-        prices = np.empty((2*self.n, self.m+1), dtype=np.float)
-        rs = np.empty((2*self.n, self.m+1), dtype=np.float)
-        for i in range(self.n):
-            prices[2*i, :], rs[2*i, :], n, p, j = self._simulate_path()
-            prices[2*i + 1, :], rs[2*i + 1, :] = self._simulate_antithetical_path(n, p, j)
-        return prices, rs
-
-    def plot_price_curve(self, type_="classical"):
-        """Plot price curve"""
-        if type_ == "antithetic":
-            prices, rs = self._simulate_price_antithetical()
-        elif type_ == "classical":
-            prices, rs = self._simulate_price()
-        mean_prices = np.mean(prices, axis=0)
-        sd_prices = np.std(prices, axis=0)
-        time_index = pd.Index(np.linspace(self.t, self.T, self.m+1))
-        time_index.name = "Year"
-        mean_prices = pd.Series(mean_prices, index=time_index)
-        sd_prices = pd.Series(sd_prices, index=time_index)
-        i_rates = pd.DataFrame(rs.T, index=time_index)
-        mean_prices.plot()
-        i_rates.plot(legend=False)
-        return prices, mean_prices, sd_prices, i_rates
+    def __init__(self, model):
+        self.model = model
+    def _generate_path(self, r0, T: float, m: int, n=None, P=None, J=None):
+        """
+        r0, float: i-rate today
+        T, float: Maturity date (in years)
+        m, int: time steps per year,
+        n, 1x(T*m) np.array, array of draws,
+        """
+        dt = 1/m
+        num_steps = int(np.ceil(T/dt))
+        r = np.empty(num_steps + 1)
+        none = False
+        if n is None:
+            n = np.empty(num_steps)
+            P = np.empty(num_steps)
+            J = np.empty(num_steps)
+            none = True
+        r[0] = r0
+        for j in range(num_steps):
+            r[j+1], n[j], P[j], J[j] = self.model.increment(rj=r[j], dt=dt, 
+                                                nj=-n[j] if not none else None,
+                                                Pj=P[j] if not none else None,
+                                                Jj=-J[j] if not none else None,
+                                                )
+        return r, n, P, J
+    def _evaluate_price(self, r: np.array, m: int):
+        """
+        r, 1xk np.array: i-rate path,
+        m, int: time steps per year
+        """
+        dt = 1/m
+        return np.exp(-np.sum(np.multiply(r, dt)))
+    def _simulate_paths(self, m: int, r0: float, n: int, T: float):
+        """
+        m, int: time steps per year
+        r0, float: i-rate today
+        n, int: number of simulations
+        T, float: Maturity date (in years)
+        """
+        prices = np.empty(n)
+        for i in range(n):
+            r, _, __, ___ = self._generate_path(r0, T, m)
+            prices[i] = self._evaluate_price(r, m)
+        return np.mean(prices), np.std(prices)
+    def _simulate_paths_anti(self, m: int, r0: float, n: int, T: float):
+        """
+        m, int: time steps per year
+        r0, float: i-rate today
+        n, int: number of simulations
+        T, float: Maturity date (in years)
+        """
+        prices = np.empty(n)
+        for i in range(n):
+            r1, n, P, J = self._generate_path(r0, T, m)
+            r2, _, __, ___ = self._generate_path(r0, T, m, n, P, J)
+            prices[i] = 0.5*(self._evaluate_price(r1, m) + self._evaluate_price(r2, m))
+        return np.mean(prices), np.std(prices)
