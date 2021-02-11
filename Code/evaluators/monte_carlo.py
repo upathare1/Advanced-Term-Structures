19fc828bfe56576b1984e72a7f6b33f08663871a
upathare <upapilot@gmail.com>
2021-02-11 13:51:27 -0800

    Add Control-Variates to MC

--- Code/evaluators/monte_carlo.py
+++ Code/evaluators/monte_carlo.py
@@ -7,13 +7,16 @@ from IPython.display import display
 class MonteCarlo:
     def __init__(self, model):
         self.model = model
-    def _generate_path(self, r0, T: float, m: int, n=None, P=None, J=None, J_pos=None):
+
+    def _generate_path(self, r0, T: float, m: int, n=None, P=None, J=None, J_pos=None, model=None):
         """
         r0, float: i-rate today
         T, float: Maturity date (in years)
         m, int: time steps per year,
         n, 1x(T*m) np.array, array of draws,
         """
+        if model is None:
+            model = self.model
         dt = 1/m
         num_steps = int(np.ceil(T/dt))
         r = np.empty(num_steps + 1)
@@ -26,11 +29,11 @@ class MonteCarlo:
             none = True
         r[0] = r0
         for j in range(num_steps):
-            r[j+1], n[j], P[j], J[j], J_pos[j] = self.model.increment(rj=r[j], dt=dt, 
-                                                nj=-n[j] if not none else None,
-                                                Pj=P[j] if not none else None,
-                                                Jj=-J[j] if not none else None,
-                                                Jj_pos=J_pos[j] if not none else None
+            r[j+1], n[j], P[j], J[j], J_pos[j] = model.increment(rj=r[j], dt=dt, 
+                                                 nj=-n[j] if not none else None,
+                                                 Pj=P[j] if not none else None,
+                                                 Jj=-J[j] if not none else None,
+                                                 Jj_pos=J_pos[j] if not none else None
                                                 )
         return r, n, P, J, J_pos
     def _evaluate_price(self, r: np.array, m: int):
@@ -40,6 +43,7 @@ class MonteCarlo:
         """
         dt = 1/m
         return np.exp(-np.sum(np.multiply(r, dt)))
+
     def _simulate_paths(self, m: int, r0: float, n: int, T: float):
         """
         m, int: time steps per year
@@ -52,6 +56,7 @@ class MonteCarlo:
             r, _, __, ___, ____ = self._generate_path(r0, T, m)
             prices[i] = self._evaluate_price(r, m)
         return np.mean(prices), np.std(prices)
+
     def _simulate_paths_anti(self, m: int, r0: float, n: int, T: float):
         """
         m, int: time steps per year
@@ -64,4 +69,21 @@ class MonteCarlo:
             r1, n, P, J, J_pos = self._generate_path(r0, T, m)
             r2, _, __, ___, ____ = self._generate_path(r0, T, m, n, P, J, J_pos)
             prices[i] = 0.5*(self._evaluate_price(r1, m) + self._evaluate_price(r2, m))
+        return np.mean(prices), np.std(prices)
+
+    def _simulate_paths_cv(self, m: int, r0: float, n: int, T: float, exact_model_class):
+        """
+        m, int: time steps per year
+        r0, float: i-rate today
+        n, int: number of simulations
+        T, float: Maturity date (in years)
+        exact_model_class, i-rate model class with exact solution
+        """
+        exact_model = exact_model_class(self.model.model_params)
+        prices = np.empty(n)
+        exact_price = exact_model.exact(r0=r0, T=T)
+        for i in range(n):
+            r_exact, _ , __, ___, ____ = self._generate_path(r0, T, m, model=exact_model)
+            r_actual, _ , __, ___, ____ = self._generate_path(r0, T, m)
+            prices[i] = exact_price + (self._evaluate_price(r_actual, m) - self._evaluate_price(r_exact, m))
         return np.mean(prices), np.std(prices)
